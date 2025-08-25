b7570147c64e1abd0563356663151abccf3c28b1
anasnorena-arch <anas.norena@gmail.com>
2025-08-24 20:41:51 -0500

    Añadiendo tareas del Día 1

---
+++ ejercicios-dia1.js
@@ -0,0 +1,7 @@
+let nombre = "sofia";
+let edad = 14;
+console.log("hola soy " + nombre + " y tengo " + edad + " años" );
+edad = 15;
+console.log("hola soy " + nombre + " y tengo " + edad + " años" );
+const pais = "Colombia";
+console.log("vivo en " + pais);
---
+++ prueba-caracteres.js
@@ -0,0 +1,12 @@
+//prueva de caracteres y simbolos de javaScript
+console.log("este string esta entre parentesis dobles");
+console.log(5+3);
+console.log(12*3);
+console.log(8-4);
+console.log(10/2);
+let (10/2) = 5;
+console.log(10%2);
+/*este
+es un
+comentario de 
+varias lineas*/
