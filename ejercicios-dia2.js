ecaa540ae6fe3243cf2f774105b12d3112fc87b2
anasnorena-arch <anas.norena@gmail.com>
2025-08-30 17:45:02 -0500

    Ejercicios Día 2 – tipos de datos y operadores

---
+++ ejercicios-dia2.js
@@ -0,0 +1,22 @@
+let nombre = "sofia";
+let edad = 15;
+let esEstudiante = true;
+let nada;
+let vacio = null;
+const pais = colombia
+console.log(typeof nombre);// string
+console.log(typeof edad);// number
+console.log(typeof esEstudiante);// boolean
+console.log(typeof nada);// undefined
+console.log(typeof vacio);// object
+let suma = edad + 5;
+console.log(suma);// 20
+let resta = edad - 4;
+console.log(resta);// 11
+let multiplication= edad * 3;
+console.log(multiplication);// 45
+let division = edad / 3;
+console.log(division);// 5
+let modulo = edad % 4;
+console.log(modulo);// 3
+console.log(`hola soy ${nombre} y tengo ${edad} años y vivo en ${pais}, soy estudiante: ${esEstudiante} y estoy aprendiendo javascript.`);
